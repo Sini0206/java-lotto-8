a3cd84eebe6ba5c19ddc84d4ade016c97490b326
Sini0206 <a01092439672@gmail.com>
2025-11-03 23:40:01 +0900

    feat: 로또 수 만큼 로또 발행

--- src/main/java/lotto/service/LottoFactory.java
+++ src/main/java/lotto/service/LottoFactory.java
@@ -3,6 +3,7 @@ package lotto.service;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.model.Lotto;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,6 +14,15 @@ public class LottoFactory {
         List<Integer> nums = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_SIZE);
         Collections.sort(nums);
         return new Lotto(nums);
+    }
+
+    public static List<Lotto> createLottos(int totalAmount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i=0; i<totalAmount; i++) {
+            lottos.add(createRandomLotto());
+        }
+
+        return lottos;
     }
 
 }
