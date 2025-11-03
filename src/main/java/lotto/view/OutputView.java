82a601e23597cc347f727996ffaa672ad89cb275
Sini0206 <a01092439672@gmail.com>
2025-11-03 23:51:37 +0900

    feat: 로또 수량 및 번호 출력

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,31 @@
+package lotto.view;
+
+import lotto.enums.ErrorMessage;
+import lotto.enums.Rank;
+import lotto.model.Lotto;
+
+import java.util.List;
+import java.util.Map;
+
+public class OutputView {
+
+    public static void printPurchasedLotto(List<Lotto> lottos) {
+        printLottoCount(lottos);
+        printLottoNumbers(lottos);
+    }
+
+    public static void printLottoCount(List<Lotto> lottos) {
+        System.out.println("\n" + lottos.size() + "개를 구매했습니다.");
+    }
+
+    public static void printLottoNumbers(List<Lotto> lottos) {
+        if (lottos.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NOT_EXIST.getMessage());
+        }
+
+        for (Lotto lotto: lottos) {
+            lotto.validate(lotto.getNumbers());
+            System.out.println(lotto.getNumbers());
+        }
+    }
+}
